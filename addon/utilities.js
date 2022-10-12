fa9aa1bbbf69085ae63addea288752f5a981163e
Sam Clopton <samsinite@gmail.com>
2022-10-12 14:29:52 -0700

    Update utilities.js

--- addon/utilities.js
+++ addon/utilities.js
@@ -11,7 +11,10 @@ export const relationShipTransform = {
   belongsTo: {
     serialize(model, key, options) {
       let relationship = model.belongsTo(key).belongsToRelationship;
-      let value = relationship.hasOwnProperty('inverseRecordData') ? relationship.inverseRecordData: relationship.canonicalState;
+      let state = relationship.canonicalState || relationship.remoteState;
+      let value = relationship.hasOwnProperty("inverseRecordData")
+        ? relationship.inverseRecordData
+        : state;
       return value && modelTransform(value, options.polymorphic);
     },
 
